28a97f8ca37db1d46125ecbf988e41b71ed84b43
ragini kumari <88733937+Raginikumari15@users.noreply.github.com>
2021-09-19 21:25:14 +0530

    Add files via upload

---
+++ main (6).cpp
@@ -0,0 +1,54 @@
+#include <iostream> //fahreinheit to celcias;
+using namespace std;
+int main()
+{
+        //int S;
+   /* int E;
+    int W;
+    int convert=0;
+    cout<<"enter S"<<endl;
+    cin>>S;
+    cout<<"enter E"<<endl;
+    cin>>E;
+    cout<<"enter W"<<endl;
+    cin>>W;
+    while(S<=E){
+      convert=(S-32)*5/9;
+        S+=W;
+    }
+    cout<<convert<<"\t";
+}*/
+    /* int S,E,W,c;
+    cin>> S >>E >>W;
+    while(S<=E){
+        c= (S-32)*5/9;
+        cout<<S <<"\t" <<c <<endl;
+      S=S+W;
+    }
+   
+    
+}*/
+int N;
+    cin >>N;
+    int i=1;
+    int j,k;
+    while(i<=N){
+        j=1;
+        k =1;
+        while(j<=i){
+            cout<<k<<endl;
+             k =k+1;
+             j= j+1;
+            
+            
+            
+        }
+    }
+  
+}
+
+
+
+
+    
+
